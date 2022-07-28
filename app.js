1900ee1db6712f62a855e4aef2762807f56646a9
Roderick <workrelax@hotmail.com>
2022-07-27 20:05:33 -0400

    DESAFIO 1

---
+++ app.js
@@ -0,0 +1,4 @@
+/* Se solicta el ingreso de un número entero, el cual, se suma el entero 10 y entrega el esultado por pantalla*/
+let numero1 = prompt('Ingresar un número entero');
+let resultado = parseInt(numero1) + 10;
+alert(resultado);
