c6d54fd8b3e473071cc5156d4a4228d394219ea3
Roderick <workrelax@hotmail.com>
2022-07-31 14:26:54 -0400

    Mejoras el HTML

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ function probarCodigo(){
 let libro;
 let facultad;
 let nombre;
-for(let i=0;i<4;i++)//Sólo se puede reservar 3 veces.
+for(let i=1;i<=3;i++)//Sólo se puede reservar 3 veces.
 {
 libro=prompt("Ingresa el libro que necesitas reservar.");
 facultad=prompt("Indica la facultad en la que estudias.");
-nombre=prompt("Indica tu nombre para poder generar la reserva del libro.");
+nombre=prompt("Indica tu nombre y apellido para poder generar la reserva del libro.");
 console.log("El libro que quieres reservar es: "+ libro);
 console.log("La facultad en la que estudias es: "+ facultad);
-console.log("El nombre quién reserva el libro es: "+ nombre);
-alert("Muchas gracias, la reserva ha quedado a nombre de: " + nombre);//Da las gracias por cada reserva
+console.log("La reserva quedó regitrada a nombre de : "+ nombre);
+alert("Muchas gracias, la reserva ha quedado registrada a nombre de: " + nombre);//Da las gracias por cada reserva
 }
 
 }
